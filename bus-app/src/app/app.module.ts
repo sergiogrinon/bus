12c32e22a015de2b072835bbbc9fb977d1a47a3e
erMiiike <tomas.vs697@gmail.com>
2021-02-13 19:18:26 +0100

    component_structure

--- bus-app/src/app/app.module.ts
+++ bus-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './app/components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { WelcomeComponent } from './components/unique/welcome/welcome.component';
+import { ContactComponent } from './components/unique/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    WelcomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
