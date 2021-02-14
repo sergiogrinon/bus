61f87bb99f41712b0c93f87b884d8aee80213f06
erMiiike <tomas.vs697@gmail.com>
2021-02-14 20:37:18 +0100

    routing_test_01
    Routing testing for welcome and contact components. Booth components works.

--- bus-app/src/app/app.module.ts
+++ bus-app/src/app/app.module.ts
@@ -1,25 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './app/components/shared/header/header.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { WelcomeComponent } from './components/unique/welcome/welcome.component';
 import { ContactComponent } from './components/unique/contact/contact.component';
 
+import { Routes, RouterModule } from '@angular/router'; // CLI imports router
+
+//la mejor idea es que la ruta sea una constante de la "clase"
+const routes: Routes = [
+  {path: 'contact', component:ContactComponent },
+  {path: 'welcome', component:WelcomeComponent },
+
+
+]; // sets up routes constant where you define your routes
+//de manera que aqui es donde tengo que decir como y cuales van a ser las rutas
+
+
+/*
+const rutas:Routes = [
+  //array de rutas.
+  {
+
+    path: 'contact',
+    Component : ContactComponent
+  }
+]
+*/
 @NgModule({
   declarations: [
+
+    //declaracion de todos los componentes que existe en mi app.
     AppComponent,
-    HeaderComponent,
     FooterComponent,
     WelcomeComponent,
     ContactComponent
   ],
+
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
